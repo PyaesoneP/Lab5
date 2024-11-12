d413c64c9a9f1e0a7f481267b433f57ec656e007
Pyae Sone <pyaesone.perfect2014@gmail.com>
2024-11-12 10:36:19 +0800

    Add led_thread

--- src/led_control.py
+++ src/led_control.py
@@ -4,3 +4,12 @@ from time import sleep
 
 from hal import hal_keypad as keypad
 
+def led_thread():
+    global delay
+    delay = 0
+    while(True):
+        if delay != 0:
+            led.set_output(20,1)
+            sleep(delay)
+            led.set_output(20,0)
+            sleep(delay)
